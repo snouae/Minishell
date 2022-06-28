85a6714dbbacc58e4bb7d2c2daaf3b483c16ada7
Soufiane Nouae <snouae@e3r12p3.1337.ma>
2022-06-28 09:56:51 +0100

    still_norm

--- srcs/builtins/builtin_root.c
+++ srcs/builtins/builtin_root.c
@@ -6,7 +6,7 @@
 /*   By: snouae <snouae@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 15:15:39 by aoumad            #+#    #+#             */
-/*   Updated: 2022/06/26 14:27:09 by snouae           ###   ########.fr       */
+/*   Updated: 2022/06/28 09:44:58 by snouae           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,44 @@
 #include <stdio.h>
 
 const struct s_builtins	g_builtins[] = {
-	{"echo", builtin_echo},
-	{"cd", builtin_cd},
-	{"pwd", builtin_pwd},
-	{"env", builtin_env},
-	{"export", builtin_export},
-	{"unset", builtin_unset},
-	{NULL, NULL},
+{"echo", builtin_echo},
+{"cd", builtin_cd},
+{"pwd", builtin_pwd},
+{"env", builtin_env},
+{"export", builtin_export},
+{"unset", builtin_unset},
+{NULL, NULL},
 };
 
-int builtin_check(char  *str)
+int	builtin_check(char *str)
 {
-    int cmd_len;
-    int is_builtin;
-    int i;
-    
-    i = 0;
-    is_builtin = 0;
-    if(!str)
-        str = ft_strdup("");
-    if (!ft_strcmp(str, "exit"))
-        is_builtin = -1;
-    while (g_builtins[i].name != NULL)
-    {
-        cmd_len = ft_strlen(g_builtins[i].name);
-        if (str && !ft_strncmp(str, g_builtins[i].name, cmd_len + 1))
-            is_builtin = i + 1; // avoid returning the value 0 since it's known for NULL in builtin_root func and not in the struct
-        i++;
-    }
-    return (is_builtin);
-}
+	int	cmd_len;
+	int	is_builtin;
+	int	i;
 
-int    builtin_root(char **argv)
+	i = 0;
+	is_builtin = 0;
+	if(!str)
+		str = ft_strdup("");
+	if (!ft_strcmp(str, "exit"))
+		is_builtin = -1;
+	while (g_builtins[i].name != NULL)
+	{
+		cmd_len = ft_strlen(g_builtins[i].name);
+		if (str && !ft_strncmp(str, g_builtins[i].name, cmd_len + 1))
+			is_builtin = i + 1;
+		i++;
+	}
+	return (is_builtin);
+}
+int	builtin_root(char **argv)
 {
-    int   i;
+	int   i;
 
-    i = builtin_check(argv[0]);
-    if (i == -1)
-        return (builtin_exit(env_count(argv), argv));
-    if (i == 0)
-        return (ERROR);
-    return (g_builtins[i - 1].func(env_count(argv), argv));                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+	i = builtin_check(argv[0]);
+	if (i == -1)
+		return (builtin_exit(env_count(argv), argv));
+	if (i == 0)
+		return (ERROR);
+	return (g_builtins[i - 1].func(env_count(argv), argv));                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 }
-
